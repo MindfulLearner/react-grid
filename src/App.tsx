0487a2ff3ed10fb640b1c63d0e67f6e52023290e
MindfulLearner <dimajosh1009@gmail.com>
2024-10-30 00:51:05 +0000

    init react layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// tailwind css
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mx-auto w-[1200px] h-[800px] mt-10 grid grid-cols-4 grid-rows-3">
+        <div className="col-span-2 row-span-1">
+          <div className="bg-red-500 h-full flex items-center justify-center font-bold text-white text-4xl">
+            1
+          </div>
+        </div>
+        <div className="col-span-2 row-span-1">
+          <div className="bg-blue-500 h-full flex items-center justify-center font-bold text-white text-4xl">
+            2
+          </div>
+        </div>
+
+        <div className="col-span-3 row-span-1">
+          <div className="bg-green-500 h-full flex items-center justify-center font-bold text-white text-4xl">
+            3
+          </div>
+        </div>
+        <div className="col-span-1 row-span-2">
+          <div className="bg-yellow-500 h-full flex items-center justify-center font-bold text-white text-4xl">
+            4
+          </div>
+        </div>
+        <div className="col-span-3 row-span-1">
+          <div className="bg-purple-500 h-full flex items-center justify-center font-bold text-white text-4xl">
+            5
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
